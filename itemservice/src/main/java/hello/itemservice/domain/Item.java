118e566a1a0183fddf92bbd7119c413a523f2321
yeahdy <yejine98@naver.com>
2024-07-23 16:28:52 +0900

    Feat: Repository JPA 로 변경 및 테스트
    - h2 추가 및 yaml 설정

--- itemservice/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice/src/main/java/hello/itemservice/domain/Item.java
@@ -1,18 +1,24 @@
 package hello.itemservice.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Item {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
-
-    public Item() {
-    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
